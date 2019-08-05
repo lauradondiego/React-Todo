7c91e1d9b70881709348c31d85c6a5509e8a033a
laura <lauradondiego@gmail.com>
2019-08-05 15:43:18 -0400

    finished task array on TodoList

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,29 @@
+import React from "react";
+
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+const taskData = [
+  {
+    task: "Walk Cat",
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: "Bake Cookies",
+    id: 1528817084358,
+    completed: false
+  },
+  {
+    task: "Netflix and Chill",
+    id: 1528817084358,
+    completed: false
+  },
+  {
+    task: "Max out Credit Card",
+    id: 1528817084358,
+    completed: false
+  }
+];
+
+export default taskData;
